2411d6ee555101bd11c9b711a1b1d4c8d0909210
ExoHayvan <18trevor3695@gmail.com>
2026-05-10 21:26:34 -0400

    Cleaning out old unneeded files (Not needed for common)

--- src/client/java/net/vassbo/vanillaemc/screen/ClientScreenHandlers.java
+++
@@ -1,9 +0,0 @@
-package net.vassbo.vanillaemc.screen;
-
-import net.minecraft.client.gui.screen.ingame.HandledScreens;
-
-public class ClientScreenHandlers {
-    public static void registerScreenHandlers() {
-        HandledScreens.register(ModScreenHandlers.DISSOLVER_SCREEN_HANDLER_TYPE, DissolverScreen::new);
-    }
-}
