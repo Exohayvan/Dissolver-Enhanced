d99be990e4e3bf5d0e3db9ce494f5e9f7f4cb221
ExoHayvan <18trevor3695@gmail.com>
2026-05-12 23:52:33 -0400

    Fixed items for 26.1.2

--- src/client/java/net/exohayvan/dissolver_enhanced/DissolverEnhancedClient.java
+++ src/client/java/net/exohayvan/dissolver_enhanced/DissolverEnhancedClient.java
@@ -1,7 +1,10 @@
 package net.exohayvan.dissolver_enhanced;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.exohayvan.dissolver_enhanced.entity.ModEntities;
 import net.exohayvan.dissolver_enhanced.packets.DataReceiverClient;
+import net.exohayvan.dissolver_enhanced.render.CrystalEntityRenderer;
 import net.exohayvan.dissolver_enhanced.screen.ClientScreenHandlers;
 
 public class DissolverEnhancedClient implements ClientModInitializer {
@@ -9,5 +12,6 @@ public class DissolverEnhancedClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		DataReceiverClient.init();
 		ClientScreenHandlers.registerScreenHandlers();
+		EntityRendererRegistry.register(ModEntities.CRYSTAL_ENTITY, CrystalEntityRenderer::new);
 	}
 }
