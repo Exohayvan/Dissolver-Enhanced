7c294cf3df512a10be017e396eb9ed490587c5c8
ExoHayvan <18trevor3695@gmail.com>
2026-05-12 23:35:48 -0400

    Fixed item registry

--- src/main/java/net/exohayvan/dissolver_enhanced/block/ModBlocks.java
+++ src/main/java/net/exohayvan/dissolver_enhanced/block/ModBlocks.java
@@ -3,9 +3,12 @@ package net.exohayvan.dissolver_enhanced.block;
 import java.util.function.ToIntFunction;
 import net.exohayvan.dissolver_enhanced.DissolverEnhanced;
 import net.exohayvan.dissolver_enhanced.item.DissolverBlockItem;
+import net.exohayvan.dissolver_enhanced.item.ModItems;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
@@ -17,14 +20,14 @@ import net.minecraft.world.level.block.state.BlockState;
 
 public class ModBlocks {
     // DISSOLVER
-    private static final Block _DISSOLVER_BLOCK = createDissolverBlock();
-    public static final DissolverBlockItem DISSOLVER_BLOCK_ITEM = new DissolverBlockItem(_DISSOLVER_BLOCK, new Item.Properties().rarity(Rarity.RARE));
+    private static final Block _DISSOLVER_BLOCK = createDissolverBlock("dissolver_block");
+    public static final DissolverBlockItem DISSOLVER_BLOCK_ITEM = new DissolverBlockItem(_DISSOLVER_BLOCK, ModItems.itemProperties("dissolver_block").rarity(Rarity.RARE));
     public static final Block DISSOLVER_BLOCK = registerBlock("dissolver_block", _DISSOLVER_BLOCK, DISSOLVER_BLOCK_ITEM);
     
     // HELPERS
 
-    private static Block createDissolverBlock() {
-        return new DissolverBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.NETHERITE_BLOCK).sound(SoundType.AMETHYST).lightLevel(getLuminance(12)).noOcclusion());
+    private static Block createDissolverBlock(String id) {
+        return new DissolverBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.NETHERITE_BLOCK).setId(blockKey(id)).sound(SoundType.AMETHYST).lightLevel(getLuminance(12)).noOcclusion());
     }
     
     private static Block registerBlock(String id, Block block, BlockItem blockItem) {
@@ -34,6 +37,10 @@ public class ModBlocks {
 
     private static Item registerBlockItem(String namespace, String id, BlockItem blockItem) {
         return Registry.register(BuiltInRegistries.ITEM, Identifier.fromNamespaceAndPath(namespace, id), blockItem);
+    }
+
+    private static ResourceKey<Block> blockKey(String id) {
+        return ResourceKey.create(Registries.BLOCK, Identifier.fromNamespaceAndPath(DissolverEnhanced.MOD_ID, id));
     }
     
     private static ToIntFunction<BlockState> getLuminance(int luminance) {
--- src/main/java/net/exohayvan/dissolver_enhanced/item/ModItems.java
+++ src/main/java/net/exohayvan/dissolver_enhanced/item/ModItems.java
@@ -2,12 +2,14 @@ package net.exohayvan.dissolver_enhanced.item;
 
 import net.exohayvan.dissolver_enhanced.DissolverEnhanced;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Item;
 
 public class ModItems {
-    public static final Item CRYSTAL_FRAME_ITEM = registerItem("crystal_frame_item", new CrystalFrameItem(new Item.Properties()));
+    public static final Item CRYSTAL_FRAME_ITEM = registerItem("crystal_frame_item", new CrystalFrameItem(itemProperties("crystal_frame_item")));
 
     // private static void addToVanillaTools(FabricItemGroupEntries entries) {
     //     entries.add(MAGIC_ITEM);
@@ -20,6 +22,14 @@ public class ModItems {
 		Item registeredItem = Registry.register(BuiltInRegistries.ITEM, itemID, item);
 
 		return registeredItem;
+    }
+
+    public static Item.Properties itemProperties(String id) {
+        return new Item.Properties().setId(itemKey(id));
+    }
+
+    private static ResourceKey<Item> itemKey(String id) {
+        return ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(DissolverEnhanced.MOD_ID, id));
     }
 
     // INITIALIZE
