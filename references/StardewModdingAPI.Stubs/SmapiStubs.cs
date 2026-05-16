881c2f04a108d46ea2b80044d02dd56d1f21874e
ExoHayvan <18trevor3695@gmail.com>
2026-05-16 19:22:45 -0400

    Make Stardew SMAPI build CI friendly

---
+++ references/StardewModdingAPI.Stubs/SmapiStubs.cs
@@ -0,0 +1,40 @@
+namespace StardewModdingAPI;
+
+public abstract class Mod
+{
+    public IMonitor Monitor { get; protected set; } = NullMonitor.Instance;
+
+    public abstract void Entry(IModHelper helper);
+}
+
+public interface IModHelper
+{
+}
+
+public interface IMonitor
+{
+    void Log(string message, LogLevel level = LogLevel.Trace);
+}
+
+public enum LogLevel
+{
+    Trace,
+    Debug,
+    Info,
+    Warn,
+    Error,
+    Alert
+}
+
+internal sealed class NullMonitor : IMonitor
+{
+    public static readonly NullMonitor Instance = new();
+
+    private NullMonitor()
+    {
+    }
+
+    public void Log(string message, LogLevel level = LogLevel.Trace)
+    {
+    }
+}
