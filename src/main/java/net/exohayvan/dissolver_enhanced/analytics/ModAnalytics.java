c09c626e63d026483664d414ca6a1ab8f0ce380a
ExoHayvan <18trevor3695@gmail.com>
2026-05-17 03:14:43 -0400

    Add connected players heartbeat property

--- src/main/java/net/exohayvan/dissolver_enhanced/analytics/ModAnalytics.java
+++ src/main/java/net/exohayvan/dissolver_enhanced/analytics/ModAnalytics.java
@@ -266,7 +266,9 @@ public final class ModAnalytics {
         properties.put("world_info", "server");
         properties.put("game_mode", "server");
         properties.put("session_location", "server");
-        properties.put("player_count", server.getPlayerCount());
+        int connectedPlayers = server.getPlayerCount();
+        properties.put("player_count", connectedPlayers);
+        properties.put("connected_players", connectedPlayers);
         properties.put("emc_storage_mode", ModConfig.PRIVATE_EMC ? "private_total" : "shared");
 
         ServerTotals totals = serverTotals(server);
